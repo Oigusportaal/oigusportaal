c200560e0a2952dc6c452cef0a1e0330bc19038c
servet kurt <servet@ut.ee>
2013-10-13 17:42:31 +0300

    test2REPO

---
+++ src/Search/GetBureauDetaildDB.java
@@ -0,0 +1,18 @@
+package Search;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+
+public class GetBureauDetaildDB  {
+	
+	public void getBureauDB (int field)
+	{
+
+	}
+	
+	
+
+}
