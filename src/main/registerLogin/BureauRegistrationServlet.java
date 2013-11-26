8cd3f5a23f47151bf6126ce48a03a557045f02d8
Urmas Muser <urmas527885@gmail.com>
2013-11-26 12:02:50 +0200

    Fix for registration

--- src/main/registerLogin/BureauRegistrationServlet.java
+++ src/main/registerLogin/BureauRegistrationServlet.java
@@ -30,7 +30,7 @@ public class BureauRegistrationServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
 		response.setContentType("text/html");
@@ -140,7 +140,7 @@ public class BureauRegistrationServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
